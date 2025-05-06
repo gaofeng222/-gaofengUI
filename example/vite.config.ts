45fd68ef784fa41100c5301a565bb1d4ddccab54
gaofeng222 <596642721@qq.com>
2025-05-06 18:31:41 +0800

    fix:修复文件引入后缀问题

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import DefineOptions from "unplugin-vue-define-options/vite";//引入
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import eslint from "vite-plugin-eslint";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import DefineOptions from "unplugin-vue-define-options/vite"; //引入
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), DefineOptions()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".vue"],
   },
-  envDir: path.resolve(__dirname, 'env'),
+  envDir: path.resolve(__dirname, "env"),
   server: {
     port: 3000,
     cors: true,
-    proxy: {}
+    proxy: {},
   },
   build: {
-    outDir: path.resolve(__dirname, '../dist')
-  }
-})
+    outDir: path.resolve(__dirname, "../dist"),
+  },
+});
